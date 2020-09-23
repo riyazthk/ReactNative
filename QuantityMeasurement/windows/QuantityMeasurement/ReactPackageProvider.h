bbbecbff60a5ffc424ed8b091e07ecf3b9a3edfd
riyazthk <er.riyaz2507@gmail.com>
2020-09-23 14:05:07 +0530

    [Riyaz]ADDED:Initial Commit

---
+++ QuantityMeasurement/windows/QuantityMeasurement/ReactPackageProvider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+
+namespace winrt::QuantityMeasurement::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::QuantityMeasurement::implementation
+
+
